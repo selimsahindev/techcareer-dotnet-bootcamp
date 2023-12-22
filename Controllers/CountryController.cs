de31e156f86e9b049fb1298635f33d727a50e2e8
Selim Şahin <selimsahin.sns@gmail.com>
2023-12-22 21:10:30 +0300

    Created the initial project, added CountryController

---
+++ Controllers/CountryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TechcareerDotnetBootcamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CountryController : Controller
+    {
+        // GET: api/country
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            List<string> countries = CountryData.GetCountries();
+            return countries;
+        }
+
+        // GET api/country/5
+        [HttpGet("{count}")]
+        public IEnumerable<string> Get(int count)
+        {
+            var countries = CountryData.GetCountries().Take(count);
+            return countries;
+        }
+    }
+}
+
---
+++ Data/CountryData.cs
@@ -0,0 +1,29 @@
+﻿public class CountryData
+{
+    public static List<string> GetCountries()
+    {
+        return new List<string>
+        {
+            "United States",
+            "Canada",
+            "United Kingdom",
+            "Germany",
+            "France",
+            "Japan",
+            "Australia",
+            "Brazil",
+            "India",
+            "China",
+            "South Africa",
+            "Mexico",
+            "Italy",
+            "Spain",
+            "Russia",
+            "Argentina",
+            "Sweden",
+            "Netherlands",
+            "South Korea",
+            "Turkey"
+        };
+    }
+}
